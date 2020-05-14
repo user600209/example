e950f3547d331d9ca85b4699bf210e5ebed32405
hisanaga <hisanaga@HIS-KIRA>
2020-05-14 18:35:55 +0900

    2回目でclass無視

--- src/Test.java
+++ src/Test.java
@@ -1,6 +1,7 @@
 
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -15,7 +16,7 @@ public class Test extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
     /**
-     * Default constructor. 
+     * Default constructor.
      */
     public Test() {
         // TODO Auto-generated constructor stub
@@ -34,6 +35,7 @@ public class Test extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		//hisanaga
 		doGet(request, response);
 	}
 
