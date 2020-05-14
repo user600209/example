1913d67500c76a2f6e4a379d001a2dc1d9b1c440
hisanaga <hisanaga@HIS-KIRA>
2020-05-14 18:46:06 +0900

    ソースだけ

--- src/Test.java
+++ src/Test.java
@@ -35,7 +35,7 @@ public class Test extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		//hisanaga
+		//hisanaga2
 		doGet(request, response);
 	}
 
